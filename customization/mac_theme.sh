0cabe058319e03ffbc147e7ddfe4ec6fc8c2e7af
andrevitalb <santiago_vital@outlook.com>
2021-12-19 17:38:25 -0600

    Added customization shell file & assets

---
+++ customization/mac_theme.sh
@@ -0,0 +1,18 @@
+apt install plank
+
+mkdir -p Documents/System/Themes/WhiteSur
+cd Documents/System/Themes/WhiteSur
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme
+git clone https://github.com/vinceliuice/WhiteSur-cursors
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme
+
+cd WhiteSur-gtk-theme
+./install.sh
+
+cp -R src/other/plank/* ~/.local/share/plank/themes
+
+cd ../WhiteSur-icon-theme
+./install.sh
+
+cd ../WhiteSur-cursors
+./install.sh
