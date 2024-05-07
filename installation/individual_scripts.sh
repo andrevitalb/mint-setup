ae06bb455733ec1b69d7cf275bae4fb46bf1da60
André Vital <andre@andrevital.com>
2024-05-07 16:53:53 -0600

    feat: May 24 updates (#5)
    * Update bashrc
    
    * Update installation scripts

--- installation/individual_scripts.sh
+++ installation/individual_scripts.sh
@@ -1,5 +1,5 @@
 #   Basic stuff
-apt install -y \
+sudo apt-get install -y \
     curl \
     apt-transport-https \
     git \
@@ -9,10 +9,10 @@ apt install -y \
 
 # Enabling/installing snapd
 rm /etc/apt/preferences.d/nosnap.pref
-apt install snapd
+sudo apt-get install snapd
 
 # Simple apt installs
-apt install -y \
+sudo apt-get install -y \
     deluge \
     locate \
     xbindkeys xautomation
@@ -20,17 +20,21 @@ apt install -y \
 # Brave browser
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=$(dpkg --print-architecture)] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-apt update
-apt install brave-browser
+sudo apt-get update
+sudo apt-get install brave-browser
 
 # Node JS
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-apt install nodejs
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# Yarn
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 # NPM basic packages
-npm i -g npm-check-updates yarn
+npm i -g npm-check-updates
 
 # Python 3.10
-apt install python3 python-is-python3
+sudo apt-get install python3 python-is-python3
 
 # Docker
 mkdir -p /etc/apt/keyrings
@@ -45,12 +49,11 @@ echo \
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # PostgreSQL
-apt update
-apt install postgresql postgresql-contrib
+sudo apt-get install postgresql postgresql-contrib
 
 # Manual PostgreSQL config
 # sudo -u postgres psql
@@ -61,8 +64,8 @@ apt install postgresql postgresql-contrib
 # PgAdmin4
 curl  -fsSL https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/pgadmin.gpg
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
-apt update -y
-apt install -y pgadmin4-desktop
+sudo apt-get update -y
+sudo apt-get install -y pgadmin4-desktop
 
 # Inkdrop
 wget https://api.inkdrop.app/download/linux/deb -O /tmp/inkdrop.deb && sudo dpkg -i /tmp/inkdrop.deb && rm /tmp/inkdrop.deb
@@ -75,7 +78,7 @@ cd alacritty
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup override set stable
 rustup update stable
-apt install -y \
+sudo apt-get install -y \
     cmake \
     pkg-config \
     libfreetype6-dev \
@@ -101,13 +104,13 @@ nano /etc/bash.bashrc
 ### Add alacritty* to the line containing xterm*|rxvt*
 
 # Snap installations
-snap install code --classic
-snap install spotify
-snap install discord
-snap install vlc
-snap install notion-snap
-snap install terraform --classic
-snap install zoom-client
-snap install teams-for-linux
-snap install todoist
-snap install bluemail
+sudo snap install code --classic
+sudo snap install spotify
+sudo snap install discord
+sudo snap install vlc
+sudo snap install notion-snap-reborn
+sudo snap install terraform --classic
+sudo snap install teams-for-linux
+sudo snap install todoist
+sudo snap install bluemail
+sudo snap install slack
